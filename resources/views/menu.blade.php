6368a7412c4c30d7e5ab2b01894796144d580395
Md. Mostak Shahid <mostak.shahid@gmail.com>
2020-06-04 22:43:39 +0600

    Initial commit

---
+++ resources/views/menu.blade.php
@@ -0,0 +1,565 @@
+User Menu
+<!-- nav -->
+<nav ui-nav class="navi clearfix">
+	<ul class="nav">
+		<li  class="active" >
+			<a href="https://backoffice.infinitemlmsoftware.com/backoffice/user/index" class="auto" >
+				<i class="fa fa-tachometer"></i>
+				<span>Dashboard</span>
+			</a>
+		</li>
+		<li>
+			<a href="javascript:void(0);" class="auto"  target="" >
+				<span class="pull-right text-muted">
+					<i class="fa fa-fw fa-angle-right text"></i>
+					<i class="fa fa-fw fa-angle-down text-active"></i>
+				</span>
+				<i class="fa fa-sitemap"></i>
+				<span>Network</span>
+			</a>
+			<ul class="nav nav-sub dk">
+				<li>
+					<a href="https://backoffice.infinitemlmsoftware.com/backoffice/user/genology_tree">
+						<span>Genealogy Tree</span>
+					</a>
+				</li>
+				<li>
+					<a href="https://backoffice.infinitemlmsoftware.com/backoffice/user/sponsor_tree">
+						<span>Sponsor Tree</span>
+					</a>
+				</li>
+				<li>
+					<a href="https://backoffice.infinitemlmsoftware.com/backoffice/user/select_tree">
+						<span>Tree View</span>
+					</a>
+				</li>
+				<li>
+					<a href="https://backoffice.infinitemlmsoftware.com/backoffice/user/binary_history">
+						<span>Downline Members</span>
+					</a>
+				</li>
+				<li>
+					<a href="https://backoffice.infinitemlmsoftware.com/backoffice/user/unilevel_history">
+						<span>Referral Members</span>
+					</a>
+				</li>
+			</ul>
+		</li>
+		<li>
+			<a href="https://backoffice.infinitemlmsoftware.com/backoffice/user_register" class="auto" >
+				<i class="fa fa-user-plus"></i>
+				<span>Register</span>
+			</a>
+		</li>
+		<li>
+			<a href="javascript:void(0);" class="auto"  target="" >
+				<span class="pull-right text-muted">
+					<i class="fa fa-fw fa-angle-right text"></i>
+					<i class="fa fa-fw fa-angle-down text-active"></i>
+				</span>
+				<i class="fa fa-briefcase"></i>
+				<span>E-Wallet</span>
+			</a>
+			<ul class="nav nav-sub dk">
+				<li>
+					<a href="https://backoffice.infinitemlmsoftware.com/backoffice/user/my_ewallet">
+						<span>E-Wallet Statement</span>
+					</a>
+				</li>
+				<li>
+					<a href="https://backoffice.infinitemlmsoftware.com/backoffice/user/fund_transfer">
+						<span>Fund Transfer</span>
+					</a>
+				</li>
+				<li>
+					<a href="https://backoffice.infinitemlmsoftware.com/backoffice/user/my_transfer_details">
+						<span>Transfer History </span>
+					</a>
+				</li>
+			</ul>
+		</li>
+		<li>
+			<a href="https://backoffice.infinitemlmsoftware.com/backoffice/user/income" class="auto" >
+				<i class="fa fa-money"></i>
+				<span>User Earnings</span>
+			</a>
+		</li>
+		<li>
+			<a href="javascript:void(0);" class="auto"  target="" >
+				<span class="pull-right text-muted">
+					<i class="fa fa-fw fa-angle-right text"></i>
+					<i class="fa fa-fw fa-angle-down text-active"></i>
+				</span>
+				<i class="fa fa-money"></i>
+				<span>Payout</span>
+			</a>
+			<ul class="nav nav-sub dk">
+				<li>
+					<a href="https://backoffice.infinitemlmsoftware.com/backoffice/user/payout_release_request">
+						<span>Request Payout</span>
+					</a>
+				</li>
+				<li>
+					<a href="https://backoffice.infinitemlmsoftware.com/backoffice/user/my_withdrawal_request">
+						<span>Payout Status</span>
+					</a>
+				</li>
+			</ul>
+		</li>
+		<li>
+			<a href="javascript:void(0);" class="auto"  target="" >
+				<span class="pull-right text-muted">
+					<i class="fa fa-fw fa-angle-right text"></i>
+					<i class="fa fa-fw fa-angle-down text-active"></i>
+				</span>
+				<i class="fa fa-address-book-o"></i>
+				<span>Profile Management</span>
+			</a>
+			<ul class="nav nav-sub dk">
+				<li>
+					<a href="https://backoffice.infinitemlmsoftware.com/backoffice/user/profile_view">
+						<span>Profile View</span>
+					</a>
+				</li>
+				<li>
+					<a href="https://backoffice.infinitemlmsoftware.com/backoffice/user/change_password">
+						<span>Change Login Password</span>
+					</a>
+				</li>
+				<li>
+					<a href="https://backoffice.infinitemlmsoftware.com/backoffice/user/change_passcode">
+						<span>Change Transaction Password</span>
+					</a>
+				</li>
+			</ul>
+		</li>
+		<li>
+			<a href="javascript:void(0);" class="auto"  target="" >
+				<span class="pull-right text-muted">
+					<i class="fa fa-fw fa-angle-right text"></i>
+					<i class="fa fa-fw fa-angle-down text-active"></i>
+				</span>
+				<i class="fa fa-bar-chart"></i>
+				<span>Reports</span>
+			</a>
+			<ul class="nav nav-sub dk">
+				<li>
+					<a href="https://backoffice.infinitemlmsoftware.com/backoffice/user/commission_report">
+						<span>Commission</span>
+					</a>
+				</li>
+				<li>
+					<a href="https://backoffice.infinitemlmsoftware.com/backoffice/user/sales_report">
+						<span>Sales</span>
+					</a>
+				</li>
+				<li>
+					<a href="https://backoffice.infinitemlmsoftware.com/backoffice/user/payout_release_report">
+						<span>Payout Released</span>
+					</a>
+				</li>
+			</ul>
+		</li>
+		<li>
+			<a href="javascript:void(0);" class="auto"  target="" >
+				<span class="pull-right text-muted">
+					<i class="fa fa-fw fa-angle-right text"></i>
+					<i class="fa fa-fw fa-angle-down text-active"></i>
+				</span>
+				<i class="fa fa-wrench"></i>
+				<span>Tools</span>
+			</a>
+			<ul class="nav nav-sub dk">
+				<li>
+					<a href="https://backoffice.infinitemlmsoftware.com/backoffice/user/download_document">
+						<span>Download Materials</span>
+					</a>
+				</li>
+				<li>
+					<a href="https://backoffice.infinitemlmsoftware.com/backoffice/user/view_news">
+						<span>News</span>
+					</a>
+				</li>
+				<li>
+					<a href="https://backoffice.infinitemlmsoftware.com/backoffice/user/feedback_view">
+						<span>Feedback</span>
+					</a>
+				</li>
+				<li>
+					<a href="https://backoffice.infinitemlmsoftware.com/backoffice/user/faq">
+						<span>FAQs</span>
+					</a>
+				</li>
+			</ul>
+		</li>
+		<li>
+			<a href="https://backoffice.infinitemlmsoftware.com/backoffice/user/mail_management" class="auto" >
+				<i class="fa fa-envelope-o"></i>
+				<span>Mailbox</span>
+			</a>
+		</li>
+		<li>
+			<a href="https://backoffice.infinitemlmsoftware.com/backoffice/logout" class="auto" >
+				<i class="fa fa-sign-out"></i>
+				<span>Logout</span>
+			</a>
+		</li>
+	</ul>
+</nav>
+<!-- nav -->
+
+Admin
+<!-- nav -->
+<nav ui-nav class="navi clearfix">
+	<ul class="nav">
+		<li  class="active" >
+			<a href="https://backoffice.infinitemlmsoftware.com/backoffice/admin/index" class="auto" >
+				<i class="fa fa-tachometer"></i>
+				<span>Dashboard</span>
+			</a>
+		</li>
+		<li>
+			<a href="javascript:void(0);" class="auto"  target="" >
+				<span class="pull-right text-muted">
+					<i class="fa fa-fw fa-angle-right text"></i>
+					<i class="fa fa-fw fa-angle-down text-active"></i>
+				</span>
+				<i class="fa fa-sitemap"></i>
+				<span>Network</span>
+			</a>
+			<ul class="nav nav-sub dk">
+				<li>
+					<a href="https://backoffice.infinitemlmsoftware.com/backoffice/admin/genology_tree">
+						<span>Genealogy Tree</span>
+					</a>
+				</li>
+				<li>
+					<a href="https://backoffice.infinitemlmsoftware.com/backoffice/admin/sponsor_tree">
+						<span>Sponsor Tree</span>
+					</a>
+				</li>
+				<li>
+					<a href="https://backoffice.infinitemlmsoftware.com/backoffice/admin/select_tree">
+						<span>Tree View</span>
+					</a>
+				</li>
+				<li>
+					<a href="https://backoffice.infinitemlmsoftware.com/backoffice/admin/binary_history">
+						<span>Downline Members</span>
+					</a>
+				</li>
+				<li>
+					<a href="https://backoffice.infinitemlmsoftware.com/backoffice/admin/unilevel_history">
+						<span>Referral Members</span>
+					</a>
+				</li>
+			</ul>
+		</li>
+		<li>
+			<a href="javascript:void(0);" class="auto"  target="" >
+				<span class="pull-right text-muted">
+					<i class="fa fa-fw fa-angle-right text"></i>
+					<i class="fa fa-fw fa-angle-down text-active"></i>
+				</span>
+				<i class="fa fa-user-plus"></i>
+				<span>Sign Up</span>
+			</a>
+			<ul class="nav nav-sub dk">
+				<li>
+					<a href="https://backoffice.infinitemlmsoftware.com/backoffice/user_register">
+						<span>Register</span>
+					</a>
+				</li>
+				<li>
+					<a href="https://backoffice.infinitemlmsoftware.com/backoffice/admin/pending_registration">
+						<span>Approval</span>
+					</a>
+				</li>
+				<li>
+					<a href="https://backoffice.infinitemlmsoftware.com/backoffice/admin/excel_register">
+						<span>Bulk Register</span>
+					</a>
+				</li>
+			</ul>
+		</li>
+		<li>
+			<a href="javascript:void(0);" class="auto"  target="" >
+				<span class="pull-right text-muted">
+					<i class="fa fa-fw fa-angle-right text"></i>
+					<i class="fa fa-fw fa-angle-down text-active"></i>
+				</span>
+				<i class="fa fa-building-o"></i>
+				<span>Business</span>
+			</a>
+			<ul class="nav nav-sub dk">
+				<li>
+					<a href="https://backoffice.infinitemlmsoftware.com/backoffice/admin/business_summary">
+						<span>Business Summary</span>
+					</a>
+				</li>
+				<li>
+					<a href="https://backoffice.infinitemlmsoftware.com/backoffice/admin/business_transactions">
+						<span>Business Transactions</span>
+					</a>
+				</li>
+			</ul>
+		</li>
+		<li>
+			<a href="javascript:void(0);" class="auto"  target="" >
+				<span class="pull-right text-muted">
+					<i class="fa fa-fw fa-angle-right text"></i>
+					<i class="fa fa-fw fa-angle-down text-active"></i>
+				</span>
+				<i class="fa fa-briefcase"></i>
+				<span>E-Wallet</span>
+			</a>
+			<ul class="nav nav-sub dk">
+				<li>
+					<a href="https://backoffice.infinitemlmsoftware.com/backoffice/admin/business_wallet">
+						<span>E-Wallet Summary</span>
+					</a>
+				</li>
+				<li>
+					<a href="https://backoffice.infinitemlmsoftware.com/backoffice/admin/my_ewallet">
+						<span>E-Wallet Statement</span>
+					</a>
+				</li>
+				<li>
+					<a href="https://backoffice.infinitemlmsoftware.com/backoffice/admin/all_transactions">
+						<span>E-Wallet Transactions</span>
+					</a>
+				</li>
+				<li>
+					<a href="https://backoffice.infinitemlmsoftware.com/backoffice/admin/balance_report">
+						<span>E-Wallet Balance</span>
+					</a>
+				</li>
+				<li>
+					<a href="https://backoffice.infinitemlmsoftware.com/backoffice/admin/fund_management">
+						<span>Fund Credit / Debit</span>
+					</a>
+				</li>
+				<li>
+					<a href="https://backoffice.infinitemlmsoftware.com/backoffice/admin/fund_transfer">
+						<span>Fund Transfer</span>
+					</a>
+				</li>
+				<li>
+					<a href="https://backoffice.infinitemlmsoftware.com/backoffice/admin/my_transfer_details">
+						<span>Transfer History </span>
+					</a>
+				</li>
+			</ul>
+		</li>
+		<li>
+			<a href="javascript:void(0);" class="auto"  target="" >
+				<span class="pull-right text-muted">
+					<i class="fa fa-fw fa-angle-right text"></i>
+					<i class="fa fa-fw fa-angle-down text-active"></i>
+				</span>
+				<i class="fa fa-money"></i>
+				<span>Payout</span>
+			</a>
+			<ul class="nav nav-sub dk">
+				<li>
+					<a href="https://backoffice.infinitemlmsoftware.com/backoffice/admin/payout_release">
+						<span>Payout Release</span>
+					</a>
+				</li>
+				<li>
+					<a href="https://backoffice.infinitemlmsoftware.com/backoffice/admin/my_withdrawal_request">
+						<span>Payout Status</span>
+					</a>
+				</li>
+			</ul>
+		</li>
+		<li>
+			<a href="javascript:void(0);" class="auto"  target="" >
+				<span class="pull-right text-muted">
+					<i class="fa fa-fw fa-angle-right text"></i>
+					<i class="fa fa-fw fa-angle-down text-active"></i>
+				</span>
+				<i class="fa fa-address-book-o"></i>
+				<span>Profile Management</span>
+			</a>
+			<ul class="nav nav-sub dk">
+				<li>
+					<a href="https://backoffice.infinitemlmsoftware.com/backoffice/admin/profile_view">
+						<span>Profile View</span>
+					</a>
+				</li>
+				<li>
+					<a href="https://backoffice.infinitemlmsoftware.com/backoffice/admin/user_account">
+						<span>Member Overview</span>
+					</a>
+				</li>
+				<li>
+					<a href="https://backoffice.infinitemlmsoftware.com/backoffice/admin/search_member">
+						<span>Members List</span>
+					</a>
+				</li>
+				<li>
+					<a href="https://backoffice.infinitemlmsoftware.com/backoffice/admin/change_password">
+						<span>Change Login Password</span>
+					</a>
+				</li>
+				<li>
+					<a href="https://backoffice.infinitemlmsoftware.com/backoffice/admin/change_passcode">
+						<span>Change Transaction Password</span>
+					</a>
+				</li>
+				<li>
+					<a href="https://backoffice.infinitemlmsoftware.com/backoffice/admin/update_pv">
+						<span>Update PV</span>
+					</a>
+				</li>
+			</ul>
+		</li>
+		<li>
+			<a href="javascript:void(0);" class="auto"  target="" >
+				<span class="pull-right text-muted">
+					<i class="fa fa-fw fa-angle-right text"></i>
+					<i class="fa fa-fw fa-angle-down text-active"></i>
+				</span>
+				<i class="fa fa-cubes"></i>
+				<span>Package</span>
+			</a>
+			<ul class="nav nav-sub dk">
+				<li>
+					<a href="https://backoffice.infinitemlmsoftware.com/backoffice/admin/membership_package">
+						<span>Membership</span>
+					</a>
+				</li>
+			</ul>
+		</li>
+		<li>
+			<a href="javascript:void(0);" class="auto"  target="" >
+				<span class="pull-right text-muted">
+					<i class="fa fa-fw fa-angle-right text"></i>
+					<i class="fa fa-fw fa-angle-down text-active"></i>
+				</span>
+				<i class="fa fa-bar-chart"></i>
+				<span>Reports</span>
+			</a>
+			<ul class="nav nav-sub dk">
+				<li>
+					<a href="https://backoffice.infinitemlmsoftware.com/backoffice/admin/admin_profile_report">
+						<span>Profile</span>
+					</a>
+				</li>
+				<li>
+					<a href="https://backoffice.infinitemlmsoftware.com/backoffice/admin/activate_deactivate_report">
+						<span>Activate Deactivate</span>
+					</a>
+				</li>
+				<li>
+					<a href="https://backoffice.infinitemlmsoftware.com/backoffice/admin/total_joining_report">
+						<span>Joining</span>
+					</a>
+				</li>
+				<li>
+					<a href="https://backoffice.infinitemlmsoftware.com/backoffice/admin/commission_report">
+						<span>Commission</span>
+					</a>
+				</li>
+				<li>
+					<a href="https://backoffice.infinitemlmsoftware.com/backoffice/admin/total_payout_report">
+						<span>Total Bonus</span>
+					</a>
+				</li>
+				<li>
+					<a href="https://backoffice.infinitemlmsoftware.com/backoffice/admin/top_earners_report">
+						<span>Top Earners</span>
+					</a>
+				</li>
+				<li>
+					<a href="https://backoffice.infinitemlmsoftware.com/backoffice/admin/sales_report">
+						<span>Sales</span>
+					</a>
+				</li>
+				<li>
+					<a href="https://backoffice.infinitemlmsoftware.com/backoffice/admin/payout_release_report">
+						<span>Payout Released</span>
+					</a>
+				</li>
+			</ul>
+		</li>
+		<li>
+			<a href="javascript:void(0);" class="auto"  target="" >
+				<span class="pull-right text-muted">
+					<i class="fa fa-fw fa-angle-right text"></i>
+					<i class="fa fa-fw fa-angle-down text-active"></i>
+				</span>
+				<i class="fa fa-wrench"></i>
+				<span>Tools</span>
+			</a>
+			<ul class="nav nav-sub dk">
+				<li>
+					<a href="https://backoffice.infinitemlmsoftware.com/backoffice/admin/upload_materials">
+						<span>Upload Materials</span>
+					</a>
+				</li>
+				<li>
+					<a href="https://backoffice.infinitemlmsoftware.com/backoffice/admin/add_news">
+						<span>News</span>
+					</a>
+				</li>
+				<li>
+					<a href="https://backoffice.infinitemlmsoftware.com/backoffice/admin/feedback_view">
+						<span>Feedback</span>
+					</a>
+				</li>
+				<li>
+					<a href="https://backoffice.infinitemlmsoftware.com/backoffice/admin/add_faq">
+						<span>FAQs</span>
+					</a>
+				</li>
+			</ul>
+		</li>
+		<li>
+			<a href="javascript:void(0);" class="auto"  target="" >
+				<span class="pull-right text-muted">
+					<i class="fa fa-fw fa-angle-right text"></i>
+					<i class="fa fa-fw fa-angle-down text-active"></i>
+				</span>
+				<i class="fa fa-cogs"></i>
+				<span>Settings</span>
+			</a>
+			<ul class="nav nav-sub dk">
+				<li>
+					<a href="https://backoffice.infinitemlmsoftware.com/backoffice/admin/general_setting">
+						<span>System Settings</span>
+					</a>
+				</li>
+				<li>
+					<a href="https://backoffice.infinitemlmsoftware.com/backoffice/admin/site_information">
+						<span>Company Profile</span>
+					</a>
+				</li>
+				<li>
+					<a href="https://backoffice.infinitemlmsoftware.com/backoffice/admin/content_management">
+						<span>Content Management</span>
+					</a>
+				</li>
+				<li>
+					<a href="https://backoffice.infinitemlmsoftware.com/backoffice/admin/mail_content">
+						<span>Mail Content</span>
+					</a>
+				</li>
+			</ul>
+		</li>
+		<li>
+			<a href="https://backoffice.infinitemlmsoftware.com/backoffice/admin/mail_management" class="auto" >
+				<i class="fa fa-envelope-o"></i>
+				<span>Mailbox</span>
+			</a>
+		</li>
+		<li>
+			<a href="https://backoffice.infinitemlmsoftware.com/backoffice/logout" class="auto" >
+				<i class="fa fa-sign-out"></i>
+				<span>Logout</span>
+			</a>
+		</li>
+	</ul>
+</nav>
+<!-- nav -->
